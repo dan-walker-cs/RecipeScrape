02441d6c23fac249183931946c6455aa1325b9b2
dan-walker-cs <dan.lawrence.walker@gmail.com>
2021-02-19 14:51:37 -0500

    - testing hub.docker integration

--- src/main/java/com/example/RecipeScrape/RecipeScrapeApplication.java
+++ src/main/java/com/example/RecipeScrape/RecipeScrapeApplication.java
@@ -3,6 +3,7 @@ package com.example.RecipeScrape;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// applciation entrypoint
 @SpringBootApplication
 public class RecipeScrapeApplication {
 
